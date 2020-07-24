16f2356681386af15433745b0586054254ff0d64
JoshyPHP <s9e.dev@gmail.com>
2020-07-24 17:49:15 +0200

    Updated highlight.js to 10.1.2

--- loader.js
+++ loader.js
@@ -222,7 +222,7 @@
 		options       = config['hljsOptions'],
 		skip          = {},
 		style         = config['hljsStyle'] || 'default',
-		url           = config['hljsUrl']   || 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.1/build/';
+		url           = config['hljsUrl']   || 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/';
 
 	// Prepare to highlight all code blocks as soon as possible
 	if (document.readyState === 'complete')
